65f421b8c1d3ef1e8c21833682507087bf6eaa05
Mahmoud <maboukhelkhal@gmail.com>
2025-05-27 11:03:05 -0700

    create a Receipt Writer

---
+++ src/main/java/com/Pluralsight/service/ReceiptWriter.java
@@ -0,0 +1,25 @@
+package com.Pluralsight.service;
+
+import com.Pluralsight.businessEntities.Order;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ReceiptWriter {
+
+    public static void saveOrderToFile(Order order) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss");
+        String time = LocalDateTime.now().format(formatter);
+        String fileName = "receipt " + time + ".txt";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            writer.write(order.getSummary()); 
+            System.out.println("Receipt saved to " + fileName);
+        } catch (IOException e) {
+            System.out.println("Error writing receipt: " + e.getMessage());
+        }
+    }
+}
